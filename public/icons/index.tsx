57bb168aa7a03e3be1c6afa3df8c39b5c2b11eca
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-11-02 15:31:20 +0200

    Add new SVG icons and update index file
    - Added box.svg, calender-line.svg, chevron-down.svg, chevron-left.svg, close-line.svg, close.svg, envelope.svg, eye-close.svg, eye.svg, group.svg, info-hexa.svg, more-dot.svg, task.svg, and time.svg to the icons directory.
    - Updated index.tsx to include imports for the new icons.

---
+++ public/icons/index.tsx
@@ -0,0 +1,23 @@
+import CloseIcon from "./close.svg";
+import GroupIcon from "./group.svg";
+import ChevronDownIcon from "./chevron-down.svg";
+import EnvelopeIcon from "./envelope.svg";
+import CalenderIcon from "./calender-line.svg";
+import EyeIcon from "./eye.svg";
+import EyeCloseIcon from "./eye-close.svg";
+import TimeIcon from "./time.svg";
+import ChevronLeftIcon from "./chevron-left.svg";
+import MoreDotIcon from "./more-dot.svg";
+
+export {
+  MoreDotIcon,
+  CloseIcon,
+  GroupIcon,
+  ChevronDownIcon,
+  EnvelopeIcon,
+  CalenderIcon,
+  EyeIcon,
+  EyeCloseIcon,
+  TimeIcon,
+  ChevronLeftIcon,
+};
