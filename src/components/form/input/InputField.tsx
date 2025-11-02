893c56e700b1a9af028ae538ef1c7d14236c541a
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-11-02 21:51:05 +0200

    feat: Add maxLength prop to Input component for better input control

--- src/components/form/input/InputField.tsx
+++ src/components/form/input/InputField.tsx
@@ -11,6 +11,7 @@ interface InputProps {
   className?: string;
   min?: string;
   max?: string;
+  maxLength?: number;
   step?: number;
   disabled?: boolean;
   success?: boolean;
@@ -31,6 +32,7 @@ const Input: FC<InputProps> = ({
   className = "",
   min = 0,
   max,
+  maxLength,
   step,
   disabled = false,
   success = false,
@@ -65,6 +67,7 @@ const Input: FC<InputProps> = ({
         onChange={onChange}
         min={min}
         max={max}
+        maxLength={maxLength}
         step={step}
         disabled={disabled}
         className={inputClasses}
