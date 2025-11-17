4495eedf5a289b49fa68b20414233ca6c4a75715
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-11-17 19:36:14 +0200

    style: Add metadata for HomePage with title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
 import HeroSection from "@/components/home/HeroSection";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Barq - Fast and Reliable Delivery Services",
+  description:
+    "Experience swift and dependable delivery services with Barq. Sign up now to get started!",
+};
 
 export default function HomePage() {
   return (
