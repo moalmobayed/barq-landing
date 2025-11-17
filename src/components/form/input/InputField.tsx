b12cdc2d7a1ba6b81fa02e9602d79376ef4d2158
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-11-17 09:09:19 +0200

    style: Remove maxLength prop from Input component for cleaner code

--- src/components/form/input/InputField.tsx
+++ src/components/form/input/InputField.tsx
@@ -11,7 +11,6 @@ interface InputProps {
   className?: string;
   min?: string;
   max?: string;
-  maxLength?: number;
   step?: number;
   disabled?: boolean;
   success?: boolean;
@@ -32,7 +31,6 @@ const Input: FC<InputProps> = ({
   className = "",
   min = 0,
   max,
-  maxLength,
   step,
   disabled = false,
   success = false,
@@ -42,17 +40,17 @@ const Input: FC<InputProps> = ({
   dir,
 }) => {
   // Determine input styles based on state (disabled, success, error)
-  let inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs text-gray-400 focus:outline-hidden focus:ring-3 bg-gray-900 text-white/90 placeholder:text-white/30 focus:border-brand-800 ${className}`;
+  let inputClasses = `h-11 w-full rounded-lg border appearance-none px-4 py-2.5 text-sm shadow-theme-xs placeholder:text-gray-400 focus:outline-hidden focus:ring-3 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 dark:focus:border-brand-800 ${className}`;
 
   // Add styles for the different states
   if (disabled) {
-    inputClasses += `cursor-not-allowed bg-gray-800 text-gray-400 border-gray-700`;
+    inputClasses += ` text-gray-500 border-gray-300 cursor-not-allowed dark:bg-gray-800 dark:text-gray-400 dark:border-gray-700`;
   } else if (error) {
-    inputClasses += `focus:ring-3 focus:ring-error-500/10 text-error-400 border-error-500`;
+    inputClasses += ` text-error-800 border-error-500 focus:ring-3 focus:ring-error-500/10  dark:text-error-400 dark:border-error-500`;
   } else if (success) {
-    inputClasses += `focus:ring-success-500/10 focus:border-success-300 text-success-400 border-success-500`;
+    inputClasses += ` text-success-500 border-success-400 focus:ring-success-500/10 focus:border-success-300  dark:text-success-400 dark:border-success-500`;
   } else {
-    inputClasses += `focus:ring-3 focus:ring-brand-500/10 border-gray-700 bg-gray-900 text-white/90 focus:border-brand-800`;
+    inputClasses += ` bg-transparent text-gray-800 border-gray-300 focus:border-brand-300 focus:ring-3 focus:ring-brand-500/10 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:focus:border-brand-800`;
   }
 
   return (
@@ -67,7 +65,6 @@ const Input: FC<InputProps> = ({
         onChange={onChange}
         min={min}
         max={max}
-        maxLength={maxLength}
         step={step}
         disabled={disabled}
         className={inputClasses}
