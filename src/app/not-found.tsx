e7dc0bc08f9da7d5d22075350ff2dab6223e2a00
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-11-17 09:08:37 +0200

    style: Improve Not Found page styling for better visibility and user experience

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,44 +1,46 @@
 import GridShape from "@/components/common/GridShape";
-import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-export const metadata: Metadata = {
-  title: "صفحة غير موجودة | برق",
-  description: "الصفحة التي تبحث عنها غير موجودة.",
-};
-
 export default function NotFound() {
   return (
-    <div className="relative z-1 flex min-h-screen flex-col items-center justify-center overflow-hidden bg-gray-900 p-6">
+    <div className="relative z-1 flex min-h-screen flex-col items-center justify-center overflow-hidden p-6">
       <GridShape />
       <div className="mx-auto w-full max-w-[242px] text-center sm:max-w-[472px]">
-        <h1 className="text-title-md xl:text-title-2xl mb-8 font-bold text-white/90">
+        <h1 className="text-title-md xl:text-title-2xl mb-8 font-bold text-gray-800 dark:text-white/90">
           خطأ
         </h1>
 
         <Image
+          src="/images/error/404.svg"
+          alt="404"
+          className="dark:hidden"
+          width={472}
+          height={152}
+        />
+        <Image
           src="/images/error/404-dark.svg"
           alt="404"
+          className="hidden dark:block"
           width={472}
           height={152}
         />
 
-        <p className="mt-10 mb-6 text-base text-gray-400 sm:text-lg">
+        <p className="mt-10 mb-6 text-base text-gray-700 sm:text-lg dark:text-gray-400">
           لا يمكننا العثور على الصفحة التي تبحث عنها!
         </p>
 
         <Link
           href="/"
-          className="shadow-theme-xs inline-flex items-center justify-center rounded-lg border border-gray-700 bg-gray-800 px-5 py-3.5 text-sm font-medium text-gray-400 hover:bg-white/3 hover:text-gray-200"
+          className="shadow-theme-xs inline-flex items-center justify-center rounded-lg border border-gray-300 bg-white px-5 py-3.5 text-sm font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
         >
           الرجوع إلى الصفحة الرئيسية
         </Link>
       </div>
 
       {/* <!-- Footer --> */}
-      <p className="absolute bottom-6 text-center text-sm text-gray-400">
+      <p className="absolute bottom-6 text-center text-sm text-gray-500 dark:text-gray-400">
         &copy; {new Date().getFullYear()} - Mohamed Tarek - جميع الحقوق محفوظة.
       </p>
     </div>
