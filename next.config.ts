e2721ef5f052ada1814b2f5b53aba57e8913c64e
Mohamed Tarek <eng.mohamedtarek0@gmail.com>
2025-11-28 16:25:23 +0200

    feat: Add next-intl structure

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
   webpack(config) {
@@ -21,4 +26,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
